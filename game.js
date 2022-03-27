56e4277facbd7ed097c00fef11bd3012586aecb2
Zach Pierce <19274781+zap5@users.noreply.github.com>
2022-03-26 17:24:45 -0700

    Ugly logic for playing a round

--- game.js
+++ game.js
@@ -18,18 +18,54 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerMove = playerSelection.toLowerCase();
-    let compMove = computerSelection;
+    const playerMove = playerSelection.toLowerCase();
+    const compMove = computerSelection;
     let outcome = '';
     let rationale = '';
 
+    const scenario1 = 'Paper beats rock.'
+    const scenario2 = 'Rock beats scissors.'
+    const scenario3 = 'Scissors beats paper.'
+
     // check if it's a tie
 
-    if (playerMove == compMove) {
+    if (playerMove === compMove) {
         outcome = 'Tie';
         rationale = 'Both players cast the same move.';
     }
 
     // if not, check to see who won
-    // return string like `You ${outcome}! ${rationale}.`
+
+    if (playerMove === 'rock') {
+        if (compMove === 'paper') {
+            outcome = 'Lose';
+            rationale = scenario1;
+        }
+        else if (compMove === 'scissors') {
+            outcome = 'Win';
+            rationale = scenario2;
+        }
+    }
+    else if (playerMove === 'paper') {
+        if (compMove === 'scissors') {
+            outcome = 'Lose';
+            rationale = scenario3;
+        }
+        else if (compMove === 'rock') {
+            outcome = 'Win';
+            rationale = scenario1;
+        }
+    }
+    else {
+        if (compMove === 'rock') {
+            outcome = 'Lose';
+            rationale = scenario2;
+        }
+        else if (compMove === 'paper') {
+            outcome = 'Win'; 
+            rationale = scenario3
+        }
+    }
+
+    return `You ${outcome}! ${rationale}.`
   }
